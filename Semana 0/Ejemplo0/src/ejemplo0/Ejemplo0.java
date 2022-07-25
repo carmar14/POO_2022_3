7710d534fad31a63a2df64599f9846ef8679d3fc
carmar14 <carmar141414@hotmail.com>
2022-07-25 16:56:40 -0500

    Nada

--- Semana 0/Ejemplo0/src/ejemplo0/Ejemplo0.java
+++ Semana 0/Ejemplo0/src/ejemplo0/Ejemplo0.java
@@ -16,6 +16,8 @@ public class Ejemplo0 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola mundo");
+        
     }
     
 }
