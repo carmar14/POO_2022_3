82e27e27e0babe393699b4b0774649c2c0ca1673
carmar14 <carmar141414@hotmail.com>
2022-07-27 12:25:19 -0500

    Ejemplo factorial
    Solución sin usar metodos

--- Semana 0/EjemploFactorial/src/ejemplofactorial/EjemploFactorial.java
+++ Semana 0/EjemploFactorial/src/ejemplofactorial/EjemploFactorial.java
@@ -5,6 +5,8 @@
  */
 package ejemplofactorial;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Acer
@@ -18,6 +20,49 @@ public class EjemploFactorial {
         // TODO code application logic here
         //---------declaracion de las variables---------
         int a = 0;
+        int b = 0;
+        int c = 0;
+        
+        double fa = 1;
+        int fb = 1;
+        int fc = 1;
+        
+        double res = 0.0;
+        
+        //---------solicito los tres numeros--------
+        a = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el valor entero a solicitado: "));
+        
+        b = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el valor entero b solicitado: "));
+        
+        c = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el valor entero c solicitado: "));
+        
+        //---------calcular factorial de cada uno-----
+                
+        for (int i = a; i > 0; i--){
+            //System.out.println("Esto es i: " + i);
+            
+            fa *= i;
+        }
+        
+        for (int i = b; i > 0; i--){
+            //System.out.println("Esto es i: " + i);
+            
+            fb *= i;
+        }
+        
+        for (int i = c; i > 0; i--){
+            //System.out.println("Esto es i: " + i);
+            
+            fc *= i;
+        }
+        
+        res = (fa+fb+fc)/fa;
+        //---------salidas------------
+        System.out.println("El factorial de a es: " + fa);
+        System.out.println("El factorial de b es: " + fb);
+        System.out.println("El factorial de c es: " + fc);
+        System.out.println("El resultado es: " + res);
+        
     }
     
 }
